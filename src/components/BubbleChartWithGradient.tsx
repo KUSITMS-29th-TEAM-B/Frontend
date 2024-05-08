9598218a838721e072c7317c63c1d065f4350c30
이효원 <ymj07168@gmail.com>
2024-05-08 21:21:03 +0900

    feat: 원 요소 이미지 추가

--- src/components/BubbleChartWithGradient.tsx
+++ src/components/BubbleChartWithGradient.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import circleS from "../assets/images/circleS.png";
+import circleM from "../assets/images/circleM.png";
+import circleL from "../assets/images/circleL.png";
 
 interface DataItem {
   label: string;
   value: number;
-  colorStart: string;
-  offsetStart: string;
-  colorEnd: string;
-  offsetEnd: string;
+  option: string;
 }
 
 const BubbleChartWithGradient = ({ data }: { data: DataItem[] }) => {
@@ -23,26 +23,6 @@ const BubbleChartWithGradient = ({ data }: { data: DataItem[] }) => {
       .attr("viewBox", "0 0 600 400")
       .attr("preserveAspectRatio", "xMidYMid meet");
 
-    const defs = svg.append("defs");
-    data.forEach((item) => {
-      const gradient = defs
-        .append("linearGradient")
-        .attr("id", `gradient${item.label}`)
-        .attr("x1", "0%")
-        .attr("y1", "0%")
-        .attr("x2", "0%")
-        .attr("y2", "100%");
-
-      gradient
-        .append("stop")
-        .attr("offset", item.offsetStart)
-        .attr("stop-color", item.colorStart);
-      gradient
-        .append("stop")
-        .attr("offset", item.offsetEnd)
-        .attr("stop-color", item.colorEnd);
-    });
-
     const pack = d3.pack().size([600, 400]).padding(10);
     const root = d3.hierarchy({ children: data }).sum((d: any) => d.value);
     const nodes = pack(root).leaves();
@@ -55,30 +35,63 @@ const BubbleChartWithGradient = ({ data }: { data: DataItem[] }) => {
       .attr("transform", (d) => `translate(${d.x},${d.y})`);
 
     node
-      .append("circle")
-      .attr("r", (d) => d.r)
-      .attr("fill", (d: any) => `url(#gradient${d.data.label})`);
+      .append("image")
+      .attr(
+        "xlink:href",
+        (d: any) =>
+          `${
+            d.data.option === "l"
+              ? circleL
+              : d.data.option === "m"
+              ? circleM
+              : circleS
+          }`
+      )
+      .attr("width", (d) => d.r * 2) // 노드 반지름의 두 배
+      .attr("height", (d) => d.r * 2)
+      .attr("x", (d) => -d.r) // 이미지를 중앙에 위치
+      .attr("y", (d) => -d.r);
 
     // 텍스트 추가, 각 tspan 요소로 이름과 값을 나눔
     node
       .append("text")
       .attr("text-anchor", "middle") // 텍스트 중앙 정렬
       .each(function (d: any) {
+        const fontSize = Math.max(10, d.r / 5);
         d3.select(this)
           .append("tspan")
           .attr("x", 0)
           .attr("dy", "-0.3em") // 첫 번째 줄 위치 조정
           .text(d.data.label)
-          .style("font-weight", "bold");
+          .style("font-weight", "bold")
+          .style("font-size", fontSize + "px");
 
         d3.select(this)
           .append("tspan")
           .attr("x", 0)
           .attr("dy", "1.2em") // 두 번째 줄 위치 조정
-          .text(d.data.value);
+          .text(d.data.value)
+          .style("font-size", fontSize + "px");
       })
-      .style("fill", "black")
-      .style("font-size", "12px");
+      .style(
+        "fill",
+        (d: any) => `${d.data.option === "l" ? "white" : "black"}`
+      );
+
+    // 마우스 이벤트를 추가하는 부분
+    node
+      .on("mouseover", function (event, d) {
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .attr("transform", `translate(${d.x},${d.y}) scale(1.1)`); // 10% 확대
+      })
+      .on("mouseout", function (event, d) {
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .attr("transform", `translate(${d.x},${d.y}) scale(1)`); // 원래 크기로 복귀
+      });
   }, [data]);
 
   return (
