e7899ceb896830961903ec78b301ef02459a7553
이효원 <ymj07168@gmail.com>
2024-05-20 06:20:09 +0900

    feat: 경험 삭제 API 연동

--- src/services/Experience/experienceApi.ts
+++ src/services/Experience/experienceApi.ts
@@ -28,7 +28,16 @@ export const patchExperience = async (
   expData: ExperienceType,
   token: string
 ) => {
-  return await client.patch(`/api/experiences/${expId}}`, expData, {
+  return await client.patch(`/api/experiences/${expId}`, expData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+// 경험 삭제
+export const deleteExperience = async (expId: string, token: string) => {
+  return await client.delete(`/api/experiences/${expId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
